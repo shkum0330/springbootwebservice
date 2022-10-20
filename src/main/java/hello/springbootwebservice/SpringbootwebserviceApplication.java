aceacbfeefb7870cc23ea5e12ff5cb3154be8630
shkum0330 <shkum0330@gmail.com>
2022-10-21 01:24:14 +0900

    Initial commit

---
+++ src/main/java/hello/springbootwebservice/SpringbootwebserviceApplication.java
@@ -0,0 +1,13 @@
+package hello.springbootwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwebserviceApplication.class, args);
+	}
+
+}
